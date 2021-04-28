9c2339b20e0303665fc63bd1b4a5eb6037df10e8
VMihail <83347281+VMihail@users.noreply.github.com>
2021-04-28 15:19:25 +0300

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Reverse {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        final Scanner in = new Scanner(System.in);
         List<List<Integer>> matrix = new ArrayList<>();
 
         while(in.hasNextLine()) {
