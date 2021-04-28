0647b9429c300e8640884f6a74537cc834f40ccd
VMihail <83347281+VMihail@users.noreply.github.com>
2021-04-28 15:18:49 +0300

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,30 @@
+package helloWorld;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Reverse {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        List<List<Integer>> matrix = new ArrayList<>();
+
+        while(in.hasNextLine()) {
+            String line = in.nextLine();
+            List<Integer> numbersInLine = new ArrayList<>();
+            Scanner sc = new Scanner(line);
+            while (sc.hasNextInt()) {
+                numbersInLine.add(sc.nextInt());
+            }
+            matrix.add(numbersInLine);
+        }
+
+        for (int i = matrix.size() - 1; i >= 0; i--) {
+            for (int j = matrix.get(i).size() - 1; j >= 0; j--) {
+                System.out.print(matrix.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
